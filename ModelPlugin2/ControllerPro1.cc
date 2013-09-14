797eddadc8ca55fe9b358e2b46cb7a76dbfc0d82
Edward <edward@ubuntu.(none)>
2013-09-13 22:24:55 -0400

    Clear up my code a little bit, adapt a new structure for this model controller

--- ModelPlugin2/ControllerPro1.cc
+++ ModelPlugin2/ControllerPro1.cc
@@ -3,6 +3,7 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 #define PI 3.1415926 
@@ -20,20 +21,8 @@ namespace gazebo
 				}
 		public: void Load (physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 				{
-					// Get all the pointers point to right objects
-					this->model = _parent;
-					this->JointWR = model->GetJoint("Right_wheel_hinge");
-					this->JointWL = model->GetJoint("Left_wheel_hinge");
-					this->JointWF = model->GetJoint("Front_wheel_hinge");
-					this->JointCB = model->GetJoint("Center_hinge");
-					// Setting the model state
-					// Setting the maximium torque of the two wheels
-					this->JointWR->SetMaxForce(0,1000);
-					this->JointWL->SetMaxForce(0,1000);
-					// Setting the maximium torque of the front wheel
-					this->JointWF->SetMaxForce(0,1000);
-					// Setting the maximium torque of the body bending joint
-					this->JointCB->SetMaxForce(0,1000);
+					// Initialize the whole system
+					SystemInitialization(_parent);
 					// Testing codes
 					this->JointWR->SetVelocity(0,0);
 					this->JointWL->SetVelocity(0,0);
@@ -42,105 +31,52 @@ namespace gazebo
 					// Event register, which will make the function be executed in each iteration
 					this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&ModelController1::OnSystemRunning, this, _1));
 				}
+		private: void SystemInitialization(physics::ModelPtr parentModel)
+				{
+					// Get all the pointers point to right objects
+					this->model = parentModel;
+					this->JointWR = model->GetJoint("Right_wheel_hinge");
+					this->JointWL = model->GetJoint("Left_wheel_hinge");
+					this->JointWF = model->GetJoint("Front_wheel_hinge");
+					this->JointCB = model->GetJoint("Center_hinge");
+					// Setting the model states
+					// Setting the maximium torque of the two wheels
+					this->JointWR->SetMaxForce(0,1000);
+					this->JointWL->SetMaxForce(0,1000);
+					// Setting the maximium torque of the front wheel
+					this->JointWF->SetMaxForce(0,1000);
+					// Setting the maximium torque of the body bending joint
+					this->JointCB->SetMaxForce(0,1000);
+					// Set the angle of the hinge in the center to zero
+					math::Angle InitialAngle(0);
+					this->JointCB->SetAngle(0, InitialAngle);
+				}
 		// Testing function
 		private: void OnSystemRunning(const common::UpdateInfo & /*_info*/)
 				{
 					double AngleValue;
 					double force;
-					// math::Angle angleOfJoint1;
-					// angleOfJoint1 = Joint1->GetAngle(2);
-					// AngleValue = angleOfJoint1.Degree();		
-					//****************************************************			
-					//common::Time TimeTmp;
-					//physics::ModelState currentModelState(this->model);
-					//TimeTmp = currentModelState.GetSimTime();
-					//AngleValue = TimeTmp.Double();
-					//***************************************************
-					//AngleValue = this->RevolutionSpeedCal(Joint1,1);
-					//cout<<"Angle:"<<AngleValue<<endl;
-					//cout<<"Rotation Speed:"<<AngleValue<<" rad/s"<<endl;
-					// math::Vector3 TmpPID(0,0,0);
-					// PIDSpeedController(Joint1,0,PI/10,TmpPID);
 					force = JointCB->GetForce(0);
 					cout<<"The Angle of the bending:"<<force<<endl;
-					// if (force > 0)
-					// {
-					// 	this->JointCB->SetVelocity(0,-0.1);
-					// }else{
-					// 	this->JointCB->SetVelocity(0,0);
-					// }
 					AngleValue = this->RevolutionSpeedCal(JointWR,0);
 					cout<<"The real rotation rate is "<<AngleValue<<" rad/s"<<endl;
 
 				}
-		// This public function is used to control the speed of joint
-		// The third parameter determine what kind of speed the user want to controll: 0 for rotation, 1 for linear, 2 for prismatic
-		// If want to use the default PID controller parameters, just pass math::Vector3::Zero to KPID
-		// If there is any errors or uncompleted information, the function will return false
-		public: bool PIDSpeedController(const physics::JointPtr JointNTBC, const int AxisIndex, const double ExpectedSpeed,
-										math::Vector3 KPID , const int SpeedType = 0, const double Radius = 0)
+		// The unit of the angle is radian
+		// In the future, this function should be defined as virtual
+		public: void SetJointAngle(physics::JointPtr CurrentJoint, int RotAxis, math::Angle AngleDesired)
 				{
-					double SpeedCurrent;
-					double TorqueIncrement;
-					static double TotalTorque;
-
-					// If PID parameters are not specified, set them to default
-					if (KPID.x==0 && KPID.y==0 && KPID.z == 0)
-					{
-						KPID = DefaultKPID;
-					}
-					if (SpeedType == 0)
-					{
-						double SpeedErrorT0;
-						static double SpeedErrorHis1T0 = 0, SpeedErrorHis2T0 = 0;
-						static double SpeedErrorAccu = 0;
-
-						SpeedCurrent = this->ComplementaryFilter(this->RevolutionSpeedCal(JointNTBC,AxisIndex));
-						cout<<"SpeedCurrent = "<<SpeedCurrent<<endl;
-						cout<<"ExpectedSpeed = "<<ExpectedSpeed<<endl;
-
-						SpeedErrorT0 = ExpectedSpeed - SpeedCurrent;
-						cout<<"SpeedErrorT0 = "<<SpeedErrorT0<<endl;
-						//cout<<"The Current Error is "<<SpeedErrorT0<<" rad/s"<<endl;
-						SpeedErrorAccu += SpeedErrorT0;
-
-						TorqueIncrement = KPID.x*SpeedErrorT0 + KPID.y*SpeedErrorAccu + KPID.z*(SpeedErrorT0-SpeedErrorHis1T0);
-						cout<<"The Torque Increment "<<TorqueIncrement<<endl;
-						SpeedErrorHis2T0 = SpeedErrorHis1T0;
-						SpeedErrorHis1T0 = SpeedErrorT0;
-					}else{
-						if (SpeedType == 1)
-						{
-							// If the function is configured to calculate linar speed for a wheel but no radiu has been specified
-							// return false and end the function
-							if (Radius == 0)
-							{
-								cout<<"Wrong radius input when calculating linear speed of a wheel."<<endl;
-								return false;
-							}else{
-
-							}
-						}else{
-							if (SpeedType == 2)
-							{
-								/* code */
-							}else{
-								cout<<"Wrong Speed Type."<<endl;
-								return false;
-							}
-						}
-					}
-
-					TotalTorque += TorqueIncrement;
-					cout<<"TotalTorque = "<<TotalTorque<<endl;
-					double TotalTorqueRef;
-					TotalTorqueRef = JointNTBC->GetForce(AxisIndex) + TorqueIncrement;
-					cout<<"TotalTorqueRef = "<<TotalTorqueRef<<endl;
-					JointNTBC->SetForce(AxisIndex,(JointNTBC->GetForce(AxisIndex) + TorqueIncrement));
-					return true;
+					CurrentJoint->SetAngle(RotAxis, AngleDesired);
+				}
+		// This function will set the rotation rate of the joint
+		// The unit of this speed is rad/s
+		// In the future, this function should be defined as virtual
+		public: void SetJointSpeed(physics::JointPtr CurrentJoint, int RotAxis, double SpeedDesired)
+				{
+					CurrentJoint->SetVelocity(RotAxis,SpeedDesired);
 				}
 		// This public function is used to calculate the angular velocity of a revolute joint
-		// This method is only valid in simulation
+		// In the future, this function should be defined as virtual
 		public: double RevolutionSpeedCal(physics::JointPtr JointNTBC, const int AxisIndex)
 				{
 					double ResSpeedRef;
@@ -178,6 +114,24 @@ namespace gazebo
 					TimeStampNowHis = TimeStampNow;
 
 					return RevSpeed;
+				}
+		// Angle calculation base on the two points
+		// This angle is in the static global frame
+		private: math::Angle AngleCalculation2Points(math::Vector2d StartPoint, math::Vector2d EndPoint)
+				{
+					double displacementX, displacementY, angleC;
+					displacementX = EndPoint.x - StartPoint.x;
+					displacementY = EndPoint.y - EndPoint.y;
+					angleC = atan2(displacementY,displacementX);
+					math::Angle ReturnAngle(angleC);
+					return ReturnAngle;
+				}
+		// Angle PID Controller
+		// DesiredAngleis in the static global frame
+		// CurrentSpeed is the rotation rate of the wheel
+		private: void AnglePIDController(math::Angle DesiredAngle, math::Angle CurrentAngle, math::Vector2d CurrentSpeed)
+				{
+
 				}
 		// A complementary filter
 		// The default coefficient of the filter is 0.9
